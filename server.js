ace2c0ee72e9c72458d6c3db3dd5e5825e060e04
Emperor Biggy <rifelinktechnology@gmail.com>
2025-04-06 17:29:56 +0100

    port update on server

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 'use strict'
 
 /*
-|---------------------------------------------------------------------------
+|--------------------------------------------------------------------------
 | Http server
-|---------------------------------------------------------------------------
+|--------------------------------------------------------------------------
+|
 | This file bootstraps Adonisjs to start the HTTP server. You are free to
-| customize the process of booting the http server.
+| customize the process of booting the HTTP server.
+|
 */
 
 const { Ignitor } = require('@adonisjs/ignitor')
-const Env = require('@adonisjs/framework/src/Env')
 
 new Ignitor(require('@adonisjs/fold'))
   .appRoot(__dirname)
   .fireHttpServer()
   .then(() => {
-    const host = Env.get('HOST', '0.0.0.0') // Default to 0.0.0.0
-    const port = Env.get('PORT', 3333) // Use the port from the environment variable
-    console.log(`App is running on http://${host}:${port}`)
+    const host = '0.0.0.0';  // Ensure the app listens on 0.0.0.0
+    const port = process.env.PORT || 3333;  // Use the port from the environment variable
+    app.listen(port, host, () => {
+      console.log(`Server is running on http://${host}:${port}`);
+    });
   })
-  .catch(console.error)
+  .catch(console.error);
