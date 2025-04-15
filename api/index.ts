09b7bb070e12bb6cb23ebb0239219fcb79a41876
Emperor Biggy <rifelinktechnology@gmail.com>
2025-04-15 12:01:10 +0100

    updte database

--- api/index.ts
+++
@@ -1,6 +0,0 @@
-// api/index.ts
-import { Request, Response } from '@adonisjs/core/build/standalone'
-
-export default async (req: Request, res: Response) => {
-  return res.json({ greeting: 'Hello world in JSON' })
-}
