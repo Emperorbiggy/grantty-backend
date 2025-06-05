4a175f4a7bc5542bf9fc356759decae6adc57d2a
Emperorbiggy <rifelinktechnology@gmail.com>
2025-06-05 11:50:41 +0100

    new corrections

--- config/cors.js
+++ config/cors.js
@@ -1,5 +1,8 @@
 module.exports = {
-  origin: true,  // Allow all origins
+  origin: [
+  'http://192.168.56.1:8080',               // Local dev
+  'https://grantty.com'  // Deployed frontend
+],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   headers: true,
   exposeHeaders: ['Authorization', 'Content-Type'],
