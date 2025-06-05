2770169e73e3d603e9290bf671aef432e448e747
Emperorbiggy <rifelinktechnology@gmail.com>
2025-06-05 12:14:11 +0100

    new corredction to cors

--- config/cors.js
+++ config/cors.js
@@ -1,8 +1,8 @@
 module.exports = {
-  origin: (origin) => origin || '*',  // fallback for no origin (like Postman)
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   headers: true,
   exposeHeaders: ['Authorization', 'Content-Type'],
-  credentials: true,
+  credentials: false, 
   maxAge: 90
 }
